94bdd7866da71fbca7284d73e6ba19e5e6270ed7
Sergey Drymchenko <2linreal@gmail.com>
2025-10-19 09:28:46 +0300

    T-014 version bumped

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 dependencyResolutionManagement {
@@ -16,6 +17,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 
